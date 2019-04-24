14fb2f6c3ebd9020c3e2cc0d327e09e2aedf866d
zhaolulu <2939918997@qq.com>
2019-04-24 08:52:16 +0800

    424技能

---
+++ 424.php
@@ -0,0 +1,47 @@
+<?php
+
+class js{
+
+    public static function day12($n){
+
+//        $num = ($n>1 ? $n + $this->day12($n-1):1);
+        $num = ($n>1 ? $n + $this->day12($n-1):1);
+        return $num;
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
