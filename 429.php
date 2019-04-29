cb1477e3b7a17e246fcc7c5502df635da40a98b5
zhaolulu <2939918997@qq.com>
2019-04-29 08:54:38 +0800

    429技能

---
+++ 429.php
@@ -0,0 +1,24 @@
+<?php
+$a = [];
+function js($array,$res){
+    $arr = $array['$ares'];
+    $arr_A = [1,3,6,9];
+    $arr_B = [2,4,5,8,20];
+    if($arr_A==$arr_B){
+        echo $a ;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
